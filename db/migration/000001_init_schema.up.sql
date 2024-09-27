f62f1f3f5c10287dcf3e2c21e2bbfbbbf6268444
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-09-27 19:32:09 -0300

    Refactor foreign key constraints in init_schema.up.sql and add TransferTx function to store.go

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -40,4 +40,3 @@ ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id"
 ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
 
 ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
-
